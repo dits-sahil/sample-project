c0f3b4ac1235abd50078505d57339011804e85b7
Sahil Verma <sahil.verma@ditstek.com>
2025-05-30 16:48:15 +0530

    added auth components

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
+import { lazy } from 'react';
+import type { RoutesConfig } from '../configs/models/router.model';
 
 
 export const allRoutes: RoutesConfig[] = [
   {
     path: '/login',
-    page: lazy(() => import('../Pages/Auth/Login')),
+    page: lazy(() => import('../pages/auth/login')),
     private: false,
     roles: [],
   },
